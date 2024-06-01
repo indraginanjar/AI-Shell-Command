113050f3784bfc55b3ccc9281170010f630aa583
Indra Ginanjar <indraginanjar@gmail.com>
2024-06-01 19:49:29 +0700

    AI Shell Command: aicommand-pre-script.example.ps1: fixing user prompt get replaced with python environment name after executing aicommand-pre-script.ps1

--- script/aicommand-pre-script.example.ps1
+++ script/aicommand-pre-script.example.ps1
@@ -1,3 +1,11 @@
-$venvActivationScriptFile = Join-Path -Path $scriptDirectory -ChildPath .venv/Scripts/Activate.ps1
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true, Position = 1)]
+    [ValidateNotNullOrEmpty()]
+    [String] $ScriptDirectory
+)
 
+$venvActivationScriptFile = Join-Path -Path $ScriptDirectory -ChildPath \.venv\Scripts\Activate.ps1
+$originalPrompt = $Prompt
 . $venvActivationScriptFile
+$Prompt = $originalPrompt
